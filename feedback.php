f063080f9cc41c35dd30de70c946242b0325180a
PKizelis <p.kizelis@gmail.com>
2020-06-10 18:25:06 +0300

    feedback initial img

--- feedback.php
+++ feedback.php
@@ -2,6 +2,9 @@
 
 
   
-
+<div class="col s12 m6 l3 left-align responsive-img">
+            <img src="images/feedback.jpg" alt="feedback nuotrauka">>
+            
+        </div>
 
 <?php include "footer.php" ?> <!-- Netrinam! -->
