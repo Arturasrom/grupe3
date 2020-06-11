469b6c21006ed911718428d0ba959f3d26ec267b
Roccara <galvydis.lukas@gmail.com>
2020-06-11 21:48:50 +0300

    pakeitimas styles

--- about.php
+++ about.php
@@ -1,7 +1,7 @@
 <?php include "header.php" ?>
 
 <div class="content">
-    <img src="https://picsum.photos/id/1/800/527?grayscale&blur=2">
+    <img src="https://picsum.photos/id/1/800/527?grayscale&blur=3">
     <h3>About us</h3>
     <div class="tabContainer">
         <div class="buttonContainer">
@@ -12,33 +12,47 @@
             <button onclick="showPanel(4,'transparent')">Povilas</button>
         </div>
         <div class="tabPanel">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br>
-            Saepe commodi amet, cum corrupti dicta quasi doloribus nisi <br>
-            reiciendis laboriosam culpa esse earum quidem architecto rem <br>
-            beatae sapiente eos illo magnam.
-        </div>
-        <div class="tabPanel">Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br>
-            Quaerat harum laboriosam veniam perspiciatis possimus odio dolore suscipit <br>
-            vel dignissimos rerum quia, reprehenderit omnis esse libero! Tempora nisi <br>
-            consectetur neque nemo!
+        <img class="profilioFoto" src="https://picsum.photos/200/300?random=1">
+            <ul></ul>
+            <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
+            <li>Saepe commodi amet, cum corrupti dicta quasi doloribus nisi</li>
+            <li>reiciendis laboriosam culpa esse earum quidem architecto rem</li>
+            <li>beatae sapiente eos illo magnam.</li>
+            </ul>
         </div>
         <div class="tabPanel">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
-            Reprehenderit fugit atque inventore maxime ad nisi fugiat? <br>
-            Soluta nisi repudiandae dolor at voluptatibus minus dignissimos, <br>
-            numquam, dolorum qui enim eum est.
+        <img class="profilioFoto" src="https://picsum.photos/200/300?random=2">
+        <ul></ul>
+            <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
+            <li>Saepe commodi amet, cum corrupti dicta quasi doloribus nisi</li>
+            <li>reiciendis laboriosam culpa esse earum quidem architecto rem</li>
+            <li>beatae sapiente eos illo magnam.</li>
+            </ul>
         </div>
         <div class="tabPanel">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>
-            Nobis nulla totam esse dolorum tenetur aperiam cumque quo <br>
-            nihil suscipit nemo, optio quam tempora accusantium omnis <br>
-            consequatur? Nihil alias officiis autem?
+        <img class="profilioFoto" src="https://picsum.photos/200/300?random=3">
+        <ul></ul>
+            <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
+            <li>Saepe commodi amet, cum corrupti dicta quasi doloribus nisi</li>
+            <li>reiciendis laboriosam culpa esse earum quidem architecto rem</li>
+            <li>beatae sapiente eos illo magnam.</li>
+            </ul>
         </div>
         <div class="tabPanel">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
-            Deserunt laudantium nam id doloribus mollitia praesentium <br>
-            vitae quia perspiciatis placeat nihil fugiat ducimus, <br>
-            doloremque magnam. Perspiciatis quas id expedita voluptate! Atque.
+        <img class="profilioFoto" src="https://picsum.photos/200/300?random=4">
+        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
+            <li>Saepe commodi amet, cum corrupti dicta quasi doloribus nisi</li>
+            <li>reiciendis laboriosam culpa esse earum quidem architecto rem</li>
+            <li>beatae sapiente eos illo magnam.</li>
+            </ul>
+        </div>
+        <div class="tabPanel">
+        <img class="profilioFoto" src="https://picsum.photos/200/300?random=5">
+            <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
+            <li>Saepe commodi amet, cum corrupti dicta quasi doloribus nisi</li>
+            <li>reiciendis laboriosam culpa esse earum quidem architecto rem</li>
+            <li>beatae sapiente eos illo magnam.</li>
+            </ul>
         </div>
     </div>
 </div>
