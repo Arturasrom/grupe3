58e7c0b47e556e68e5d2a0f729b1303a6ca3d674
Arturasrom <romanovskis.arturas@gmail.com>
2020-06-11 12:09:39 +0300

    news page pakeitimai

--- news.php
+++ news.php
@@ -3,7 +3,6 @@
 
 <div class="container">
 
-
     <h1 class="news-h1">News</h1>
 
     <div class="divider"></div>
@@ -12,7 +11,7 @@
         <div class="card-panel grey lighten-5 z-depth-1">
             <div class="row valign-wrapper">
                 <div class="col s2">
-                    <img src="images/vidmantas.jpg" alt="" class="circle responsive-img">
+                    <img id="img1" src="images/vidmantas.jpg" alt="" class="circle responsive-img">
                 </div>
 
                 <div class="col s10">
@@ -20,7 +19,8 @@
                         <h5 class="news-h5">Truth can only be found in one place: the C# code.</h5>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus dolorum iure, quos odit qui animi illum quisquam optio minus cupiditate facere quas consequuntur quam. Sequi mollitia nam natus corrupti excepturi!</p>
                         <div class="news-button">
-                            <a href="#!" class="btn waves-effect waves-teal">Send</a>
+                        <button class="button-news" onclick="document.getElementById('img1').src='images/vidmantas_ty.png'">Pasakyti AČIŪ!</button>
+                        
                         </div>
                     </span>
                 </div>
@@ -28,20 +28,18 @@
         </div>
     </div>
 
-
-    
     <div class="col s12 m8 offset-m2 l6 offset-l3">
         <div class="card-panel grey lighten-5 z-depth-1">
             <div class="row valign-wrapper">
                 <div class="col s2">
-                    <img src="images/viktorija.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                    <img id="img2" src="images/viktorija.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
                 </div>
                 <div class="col s10">
                     <span class="black-text">
                         <h5 class="news-h5">Make it work, Make it right, Make it fast with JAVA.</h5>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus dolorum iure, quos odit qui animi illum quisquam optio minus cupiditate facere quas consequuntur quam. Sequi mollitia nam natus corrupti excepturi!</p>
                         <div class="news-button">
-                            <a href="#!" class="btn waves-effect waves-teal">Send</a>
+                        <button class="button-news" onclick="document.getElementById('img2').src='images/viktorija_ty.png'">Pasakyti AČIŪ!</button>
                         </div>
                     </span>
                 </div>
@@ -55,24 +53,22 @@
         <div class="card-panel grey lighten-5 z-depth-1">
             <div class="row valign-wrapper">
                 <div class="col s2">
-                    <img src="images/kazimiras.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                    <img id="img3" src="images/kazimiras.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
                 </div>
                 <div class="col s10">
                     <span class="black-text">
                         <h5 class="news-h5">Things aren’t always #000000 and #FFFFFF - learn why.</h5>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus dolorum iure, quos odit qui animi illum quisquam optio minus cupiditate facere quas consequuntur quam. Sequi mollitia nam natus corrupti excepturi</p>
                         <div class="news-button">
-                            <a href="#!" class="btn waves-effect waves-teal">Send</a>
+                        <button class="button-news" onclick="document.getElementById('img3').src='images/kazimiras_ty.png'">Pasakyti AČIŪ!</button>
                         </div>
                     </span>
                 </div>
-
             </div>
         </div>
     </div>
 
 </div>
-
 
 
 <?php include "footer.php" ?>
--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', function() {
 /* Artūro  kodas: */
 
 
+ 
+
+
 
 /* Aušros  kodas: */
 document.addEventListener('DOMContentLoaded', function() {
