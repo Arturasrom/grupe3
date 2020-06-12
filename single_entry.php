7938db5fb36ccc160e7786d28da56c54c2b03c6a
Ausra-008 <valyte.ausra@gmail.com>
2020-06-12 20:32:07 +0300

    done

--- single_entry.php
+++ single_entry.php
@@ -1,104 +1,100 @@
-
 <?php include "header.php" ?>
 <div class="journey-on">
-<img src="images/journey.jpg" alt="">
-<h1 class="journey-text"><i class="indigo-text darken-4 large material-icons">map</i>Your journey awaits</h1>
+  <img src="images/journey.jpg" alt="">
+  <h1 class="journey-text"><i class="indigo-text darken-4 large material-icons">map</i>Your journey awaits</h1>
 </div>
 
 
 <div class="Background">
 
-<div class="container">
-<ul class="breadcrumb">
-  <li><a href="http://localhost/grupe3/index.php">Home</a></li>
-  <li><a href="http://localhost/grupe3/about.php">About</a></li>
- 
-</ul>
-<div class="Background">
-  <img src="C:\xampp\htdocs\grupe3\images\image_parallax3.jpg" alt="fonas">
+  <div class="container">
+    <ul class="breadcrumb">
+      <li><a href="http://localhost/grupe3/index.php">Home</a></li>
+      <li><a href="http://localhost/grupe3/about.php">About</a></li>
 
-<div class="container">
+    </ul>
+    <div class="Background">
 
 
+      <div class="container">
 
-<div class="headeris">
+        <div class="card">
+          <div class="video-container">
+            <iframe width="420" height="345" src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder="0" allowfullscreen></iframe>
+          </div>
+          <div class="card-body">
+            <p class="card-text">With close to 40 programs taught in English and over 100 taught in French, we are dedicated to providing high-quality education in the fields of business, the arts, cinema, culture, design, theater and many more. With decades of experience, our schools offer students academic degrees with a professional twist giving them a head start in today’s increasingly competitive work force.
 
-</div>
-<div class="card" >
-<div class="video-container">
-<iframe width="420" height="345" src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder="0" allowfullscreen></iframe>
-</div>
-  <div class="card-body">
-    <p class="card-text">With close to 40 programs taught in English and over 100 taught in French, we are dedicated to providing high-quality education in the fields of business, the arts, cinema, culture, design, theater and many more. With decades of experience, our schools offer students academic degrees with a professional twist giving them a head start in today’s increasingly competitive work force.
+              Today our focus is on providing students the world over with endless study options here in the home of one of the world’s most well-known and respected cultures. When you study with Studialis, not only do you have an extensive selection of programs to choose from, you also get the luxury of experiencing the French way of life.
 
-Today our focus is on providing students the world over with endless study options here in the home of one of the world’s most well-known and respected cultures. When you study with Studialis, not only do you have an extensive selection of programs to choose from, you also get the luxury of experiencing the French way of life. 
+              Since international students often have different questions and concerns than students already located in France, the head International Office at Studialis is here to facilitate the admissions process for students coming from other countries. We handle international student applications for all schools within our group.
 
-Since international students often have different questions and concerns than students already located in France, the head International Office at Studialis is here to facilitate the admissions process for students coming from other countries. We handle international student applications for all schools within our group.
+              We’re here to advise you on the program that best fits your background, goals and French study dreams.
 
-We’re here to advise you on the program that best fits your background, goals and French study dreams.
-
-Explore our site, discover our programs and come join us!
-    </p>
-    
-  </div>
-</div>
-            
-<div class="boxed">
-
-  <div class="komanda">
-
-       
-    
-      <div class="card horizontal mycard">
-        <div class="card-image">
-          <img src="https://picsum.photos/500/500">
-        </div>
-        <div class="card-stacked">
-          <div class="card-content">
-            <h2>
-            KAZIMIRAS
-            </h2>
-            <p>Sometimes while having lessons we order pizza <a href="https://www.vilniuscoding.lt/">See yourself!</a>
-            <br>Some more text<a href="https://www.vilniuscoding.lt/">discover yourself!</a>
+              Explore our site, discover our programs and come join us!
             </p>
-            <p2>
-              Some more textSome more textSome more text
-              </p2>
-          </div>
-        </div>
-      </div>
-    
-              
 
-          
-    
-      <div class="card horizontal mycard">
-        <div class="card-image">
-          <img src="https://picsum.photos/500/500">
+          </div>
         </div>
-        <div class="card-stacked">
-          <div class="card-content">
-            <h2>
-            VIDMANTAS
-            </h2>
-            <div class="row">
-            <p>Some cool motivational words to atrract new students  <a href="https://www.vilniuscoding.lt/">discover yourself!</a>
-              <br>Some more text <a href="https://www.vilniuscoding.lt/">discover yourself!</a></p>
-              <p2>
-              Some more textSome more textSome more text
-              </p2>
-            
+
+        <div class="boxed">
+
+          <div class="komanda">
+
+
+
+            <div class="card horizontal mycard">
+              <div class="card-image">
+                <img src="https://picsum.photos/500/500">
+              </div>
+              <div class="card-stacked">
+                <div class="card-content">
+                  <h3>
+                    KAZIMIRAS
+                  </h3>
+                  <p>Sometimes while having lessons we order pizza <a href="https://www.vilniuscoding.lt/">See yourself!</a>
+                    <br>Some more text<a href="https://www.vilniuscoding.lt/">discover yourself!</a>
+                  </p>
+                  <p2>
+                    Some more textSome more textSome more text
+                  </p2>
+                </div>
+              </div>
             </div>
+
+
+
+
+
+            <div class="card horizontal mycard">
+              <div class="card-image">
+                <img src="https://picsum.photos/500/500">
+              </div>
+              <div class="card-stacked">
+                <div class="card-content">
+                  <h3>
+                    VIDMANTAS
+                  </h3>
+                  <div class="row">
+                    <p>Some cool motivational words to atrract new students <a href="https://www.vilniuscoding.lt/">discover yourself!</a>
+                      <br>Some more text <a href="https://www.vilniuscoding.lt/">discover yourself!</a></p>
+                    <p2>
+                      Some more textSome more textSome more text
+                    </p2>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
           </div>
         </div>
       </div>
-    
-            
+    </div>
   </div>
 </div>
-  
-</div>
-</div>            
+
 
 
 
