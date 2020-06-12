ea18e55b9438138a457c8b385cb57aac661862d1
Arturasrom <romanovskis.arturas@gmail.com>
2020-06-12 07:26:53 +0300

    newsphp pakeitimai

--- news.php
+++ news.php
@@ -21,13 +21,13 @@
                         <h4 class="news-h4">Truth can only be found in one place: the C# code.</h4>
                         <h6>Stuff we learned:</h6><br><br>
                         <div class="tooltips_news">
-                            <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="Data types, variables and so on...">Programming basics</a>
+                            <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="top" data-tooltip="Data types, variables and so on...">Programming basics</a>
                             <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="if else... else if if..  ">Conditions and Statements</a>
-                            <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="While (true) - loops forever">Loops</a>
+                            <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="top" data-tooltip="While (true) - loops forever">Loops</a>
                             <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="+ - : * % ++ -- /">Operators</a>
-                            <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="Switch() {case x: break; case y: break;}">Switch</a>
+                            <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="top" data-tooltip="Switch() {case x: break; case y: break;}">Switch</a>
                             <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="int[] array = {I, love, coding} ">Arrays</a>
-                            <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="enum Numbers {1, 2, 3}">Enums</a><br><br>
+                            <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="top" data-tooltip="enum Numbers {1, 2, 3}">Enums</a><br><br>
                         </div>
                         <div class="news-button">
                             <button class="button-news btn-floating btn-large blue-grey lighten-2 pulse" onclick="document.getElementById('img1').src='images/vidmantas_ty.png'">AČIŪ!</button>
@@ -53,14 +53,14 @@
                     <span class="black-text">
                         <h4 class="news-h4">Make it work, Make it right, Make it fast with JAVA.</h4>
                         <h6>Stuff we learned:</h6><br><br>
-                        <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="Just joking :)">String is the King</a>
+                        <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="top" data-tooltip="Just joking :)">String is the King</a>
                         <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="Int becomes Integer  ">Autoboxing</a>
-                        <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="We are ready for that job interview!">FIZZ BUZZ FIZZBUZZ</a>
-                        <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="List, Set, Queue, Map">Collection</a><br><br>
+                        <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="top" data-tooltip="We are ready for that job interview!">FIZZ BUZZ FIZZBUZZ</a>
+                        <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="top" data-tooltip="List, Set, Queue, Map">Collection</a>
                         <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="CSV student = new CSV ();">Objects</a>
-                        <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="Child class inherits from parent class">Inheritance</a>
+                        <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="top" data-tooltip="Child class inherits from parent class">Inheritance</a>
                         <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="Classes are related by inheritance">Polymorphism</a>
-                        <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="@Override">Overriding</a><br><br>
+                        <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="top" data-tooltip="@Override">Overriding</a><br><br>
                         <div class="news-button">
                             <button class="button-news btn-floating btn-large blue-grey lighten-2 pulse" onclick="document.getElementById('img2').src='images/viktorija_ty.png'">AČIŪ!</button>
 
@@ -90,8 +90,8 @@
                         <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="top" data-tooltip="Style in HTML. Fashion in CSS. Attitude in JavaScript.">JavaScript</a>
                         <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="Materializecss, Bootstrap, ZURB Foundation.">Frameworks</a>
                         <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="top" data-tooltip="Server side scripting language!">PHP</a>
-                        <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="Open-source cross-platform!">XAMPP</a><br><br>
-                        <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="Don't mix the sequence!">Stage All-->Commit-->Pull-->Push</a>
+                        <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="bottom" data-tooltip="Open-source cross-platform!">XAMPP</a>
+                        <a class="btn tooltipped #90a4ae blue-grey lighten-2 " data-position="top" data-tooltip="Don't mix the sequence!">Stage All-->Commit-->Pull-->Push</a>
 
                         <div class="news-button">
                             <button class="button-news btn-floating btn-large blue-grey lighten-2 pulse" onclick="document.getElementById('img3').src='images/kazimiras_ty.png'">AČIŪ!</button>
