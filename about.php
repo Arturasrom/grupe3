7c25ead5b773588b3e9c89b03ebea9e7f3480206
Roccara <galvydis.lukas@gmail.com>
2020-06-12 15:41:07 +0300

    about stilius

--- about.php
+++ about.php
@@ -12,47 +12,40 @@
             <button onclick="showPanel(4,'transparent')">Povilas</button>
         </div>
         <div class="tabPanel">
-        <img class="profilioFoto" src="https://picsum.photos/400/150?random=1">
-            <ul></ul>
-            <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
-            <li>Saepe commodi amet, cum corrupti dicta quasi doloribus nisi</li>
-            <li>reiciendis laboriosam culpa esse earum quidem architecto rem</li>
-            <li>beatae sapiente eos illo magnam.</li>
-            </ul>
+            <img class="profilioFoto" src="https://picsum.photos/200/200?random=1">
+                <li>Lorem ipsum dolor, sit amet consectetur <br>    adipisicing elit.</li>
+                <li>Saepe commodi amet, cum corrupti dicta <br>     quasi doloribus nisi</li>
+                <li>reiciendis laboriosam culpa esse earum <br>     quidem architecto rem</li>
+                <li>beatae sapiente eos illo magnam.</li>
         </div>
         <div class="tabPanel">
-        <img class="profilioFoto" src="https://picsum.photos/400/150?random=2">
-        <ul></ul>
-            <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
-            <li>Saepe commodi amet, cum corrupti dicta quasi doloribus nisi</li>
-            <li>reiciendis laboriosam culpa esse earum quidem architecto rem</li>
-            <li>beatae sapiente eos illo magnam.</li>
-            </ul>
+            <img class="profilioFoto" src="https://picsum.photos/200/200?random=2">
+                <li>Lorem ipsum dolor, sit amet consectetur <br>    adipisicing elit.</li>
+                <li>Saepe commodi amet, cum corrupti dicta <br>     quasi doloribus nisi</li>
+                <li>reiciendis laboriosam culpa esse earum <br>     quidem architecto rem</li>
+                <li>beatae sapiente eos illo magnam.</li>
         </div>
         <div class="tabPanel">
-        <img class="profilioFoto" src="https://picsum.photos/400/150?random=3">
-        <ul></ul>
-            <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
-            <li>Saepe commodi amet, cum corrupti dicta quasi doloribus nisi</li>
-            <li>reiciendis laboriosam culpa esse earum quidem architecto rem</li>
-            <li>beatae sapiente eos illo magnam.</li>
-            </ul>
+            <img class="profilioFoto" src="https://picsum.photos/200/200?random=3">
+                <li>Lorem ipsum dolor, sit amet consectetur <br>    adipisicing elit.</li>
+                <li>Saepe commodi amet, cum corrupti dicta <br>     quasi doloribus nisi</li>
+                <li>reiciendis laboriosam culpa esse earum <br>     quidem architecto rem</li>
+                <li>beatae sapiente eos illo magnam.</li>
         </div>
         <div class="tabPanel">
-        <img class="profilioFoto" src="https://picsum.photos/400/150?random=4">
-        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
-            <li>Saepe commodi amet, cum corrupti dicta quasi doloribus nisi</li>
-            <li>reiciendis laboriosam culpa esse earum quidem architecto rem</li>
-            <li>beatae sapiente eos illo magnam.</li>
-            </ul>
+            <img class="profilioFoto" src="https://picsum.photos/200/200?random=4">
+                <li>Lorem ipsum dolor, sit amet consectetur <br>    adipisicing elit.</li>
+                <li>Saepe commodi amet, cum corrupti dicta <br>     quasi doloribus nisi</li>
+                <li>reiciendis laboriosam culpa esse earum <br>     quidem architecto rem</li>
+                <li>beatae sapiente eos illo magnam.</li>
+
         </div>
         <div class="tabPanel">
-        <img class="profilioFoto" src="https://picsum.photos/400/150?random=5">
-            <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
-            <li>Saepe commodi amet, cum corrupti dicta quasi doloribus nisi</li>
-            <li>reiciendis laboriosam culpa esse earum quidem architecto rem</li>
-            <li>beatae sapiente eos illo magnam.</li>
-            </ul>
+            <img class="profilioFoto" src="https://picsum.photos/200/300?random=5">
+                <li>Lorem ipsum dolor, sit amet consectetur <br>    adipisicing elit.</li>
+                <li>Saepe commodi amet, cum corrupti dicta <br>     quasi doloribus nisi</li>
+                <li>reiciendis laboriosam culpa esse earum <br>     quidem architecto rem</li>
+                <li>beatae sapiente eos illo magnam.</li>
         </div>
     </div>
 </div>
