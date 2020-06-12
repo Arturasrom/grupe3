c32f335b3ff2aad790cb7d5a931cb2b4f80232c4
Roccara <galvydis.lukas@gmail.com>
2020-06-12 17:32:41 +0300

    Puslapis about

--- about.php
+++ about.php
@@ -5,35 +5,35 @@
     <h1 class="title2"><i class="indigo-text darken-4 large material-icons">group</i>About us</h1>
     <div class="tabContainer">
         <div class="buttonContainer">
-            <button onclick="showPanel(0,'transparent')">Artūras</button>
-            <button onclick="showPanel(1,'transparent')">Aušra</button>
-            <button onclick="showPanel(2,'transparent')">Lukas</button>
-            <button onclick="showPanel(3,'transparent')">Andrius</button>
-            <button onclick="showPanel(4,'transparent')">Povilas</button>
+            <button onclick="showPanel(0,'transparent')"><img src="images/1.jpg"> Artūras</button>
+            <button onclick="showPanel(1,'transparent')"><img src="images/5.jpg"> Aušra</button>
+            <button onclick="showPanel(2,'transparent')"><img src="images/3.jpg"> Lukas</button>
+            <button onclick="showPanel(3,'transparent')"><img src="images/4.jpg"> Andrius</button>
+            <button onclick="showPanel(4,'transparent')"><img src="images/2.jpg"> Povilas</button>
         </div>
         <div class="tabPanel">
-            <img class="profilioFoto" src="images/1.jpg">
+            <img src="images/1.jpg">
             <li>Amžius: <br> adipisicing elit.</li>
             <li>Profesija: <br> quasi doloribus nisi</li>
             <li>Hobiai: <br> quidem architecto rem</li>
             <li>Kodėl programavimas?: <br> mhmhmhmh</li>
         </div>
         <div class="tabPanel">
-            <img class="profilioFoto" src="images/5.jpg">
+            <img src="images/5.jpg">
             <li>Amžius: <br> adipisicing elit.</li>
             <li>Profesija: <br> quasi doloribus nisi</li>
             <li>Hobiai: <br> quidem architecto rem</li>
             <li>Kodėl programavimas?: <br> mhmhmhmh</li>
         </div>
         <div class="tabPanel">
-            <img class="profilioFoto" src="images/3.jpg">
+            <img src="images/3.jpg">
             <li>Amžius: 27m.</li>
             <li>Profesija: Mechanikos inžinerija.</li>
             <li>Hobiai: Sportas, motosportas.</li>
             <li>Kodėl programavimas? Po 9 metų nusprendžiau, <br> kad vis tik noriu programuoti</li>
         </div>
         <div class="tabPanel">
-            <img class="profilioFoto" src="images/4.jpg">
+            <img src="images/4.jpg">
             <li>Amžius: <br> adipisicing elit.</li>
             <li>Profesija: <br> quasi doloribus nisi</li>
             <li>Hobiai: <br> quidem architecto rem</li>
@@ -41,7 +41,7 @@
 
         </div>
         <div class="tabPanel">
-            <img class="profilioFoto" src="images/2.jpg">
+            <img src="images/2.jpg">
             <li>Amžius: <br> adipisicing elit.</li>
             <li>Profesija: <br> quasi doloribus nisi</li>
             <li>Hobiai: <br> quidem architecto rem</li>
